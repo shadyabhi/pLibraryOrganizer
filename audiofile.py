483b2646849e04423edf1cb887b1ffd802af9122
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2013-11-03 09:10:30 +0530

    Now supports python2-eyed3 version 0.7.3
    Scumbag Library: Changes it's API!

--- audiofile.py
+++ audiofile.py
@@ -1,38 +1,37 @@
 #Author: abhijeet.1989@gmail.com (shadyabhi)
 try:
-    import eyeD3
+    import eyed3
 except ImportError:
     print("You don't have eyeD3 installed. Please install that.")
 
 class AudioFile:
     def __init__(self, file_location):
         self.file_location = file_location
-        self.tag = eyeD3.Tag()
-        self.tag.link(file_location)
+        self.tag = eyed3.load(file_location).tag
     
     def getArtist(self):
-        return self.tag.getArtist()
+        return self.tag.artist
     
     def getAlbum(self):
-        return self.tag.getAlbum()
+        return self.tag.album
     
     def getYear(self):
-        return self.tag.getYear()
+        return self.tag.t.recording_date.year
 
     def getTitle(self):
-        return self.tag.getTitle()
+        return self.tag.title
     
     def getLocation(self):
         return self.file_location
 
     def setArtist(self, artist):
-        self.tag.setArtist(artist)
-        self.tag.update()
+        self.tag.artist = artist
+        self.tag.save()
 
     def setAlbum(self, album):
-        self.tag.setAlbum(album)
-        self.tag.update()
+        self.tag.album = album
+        self.tag.save()
     
     def setTitle(self, title):
-        self.tag.setTitle(title)
-        self.tag.update()
+        self.tag.title = title
+        self.tag.save()
